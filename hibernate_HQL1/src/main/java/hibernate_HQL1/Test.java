b510f1f171c62500428c69a186cce70a5accf424
notthefakekowsik <kowsik02@gmail.com>
2022-12-08 17:43:04 +0530

    HQL_Query_Select

---
+++ hibernate_HQL1/src/main/java/hibernate_HQL1/Test.java
@@ -0,0 +1,48 @@
+package hibernate_HQL1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.hibernate.Transaction;
+
+import java.util.*;
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		Alien kowshik = new Alien();
+		kowshik.setAge(22);
+		kowshik.setName("kowshik");
+		
+
+		
+		Configuration cfg = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class);
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		Transaction txc = session.beginTransaction();
+		
+		//session.persist(kowshik);
+		
+//		for(int i = 0;i<50;i++)
+//		{
+//			Alien curr = new Alien();
+//			curr.setAge(i);
+//			curr.setName("kowshik");
+//			session.persist(curr);
+//		}
+		
+		Query q = session.createQuery("from alienTable where id < 50");
+		List<Alien> aliens= q.list();
+		
+		for(Alien curr : aliens)
+		{
+			System.out.println(curr.getId()+ " "+curr.getName());
+		}
+		txc.commit();
+	}
+}
+  
